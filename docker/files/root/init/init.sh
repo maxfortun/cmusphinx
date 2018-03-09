0139d24297e0126f16b5bfc5b4cdf4d8ed258e13
Max <max@maxf.net>
2018-03-09 12:11:37 -0500

    Download models

--- docker/files/root/init/init.sh
+++ docker/files/root/init/init.sh
@@ -24,11 +24,34 @@ for p in sphinxbase sphinxtrain pocketsphinx; do
 	popd
 done
 
+pushd /usr/local/share/pocketsphinx/model
+
+for model in \
+	"US English/cmusphinx-en-us-5.2.tar.gz" \
+	"US English/cmusphinx-en-us-semi-5.1.tar.gz" \
+	"US English/cmusphinx-en-us-semi-full-5.1.tar.gz" \
+	"US English/en-70k-0.2-pruned.lm.gz" \
+	"US English/cmusphinx-en-us-8khz-5.2.tar.gz" \
+	"US English/en-70k-0.2.lm.gz" \
+	"Russian/cmusphinx-ru-5.2.tar.gz" \
+	"Russian/zero_ru_cont_8k_v3.tar.gz" ; do
+
+		curl -O -C - "https://downloads.sourceforge.net/project/cmusphinx/Acoustic%20and%20Language%20Models/$model"
+done
+
+for model in *.gz; do
+	gunzip "$model"
+done
+
+for model in *.tar; do
+	tar xvf "$model" && rm -f "$model"
+done
+
 # on docker side
 # socat tcp-listen:3643,reuseaddr,fork - | pocketsphinx_continuous -hmm /usr/local/share/pocketsphinx/model/en-us/en-us -lm /usr/local/share/pocketsphinx/model/en-us/en-us.lm.bin -dict /usr/local/share/pocketsphinx/model/en-us/cmudict-en-us.dict -infile /dev/stdin -logfn /var/log/pocketsphinx_continuous.log
 a
 # on host side
-# sox -d -c 1 -r 16k -e signed -b 16 -t wav - |  socat - tcp-connect:localhost:33643
+# sox -d -c 1 -r 16k -e signed -b 16 -L -t wav - |  socat - tcp-connect:localhost:33643
 
 
 # https://stackoverflow.com/questions/43312975/record-sound-on-ubuntu-docker-image
